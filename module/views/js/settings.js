b91741a91ecb08956392dd0da5b004ad76bd8cd1
Ulrik Moe <ulrik.moe@gmail.com>
2024-12-11 17:21:02 +0100

    Convert jquery to JS

--- module/views/js/settings.js
+++ module/views/js/settings.js
@@ -4,13 +4,15 @@
  * @license   https://opensource.org/licenses/MIT MIT License
  */
 
-$(document).ready(function () {
-    $(document).bind('click', function () {
-        $('#scanpay--captureonorderstatus--dropdown').removeClass('scanpay--show');
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdown = document.getElementById('scanpay--captureonorderstatus--dropdown');
+
+    document.addEventListener('click', function () {
+        dropdown.classList.remove('scanpay--show');
     });
 
-    $('#scanpay--captureonorderstatus--add').bind('click', function (e) {
-        $('#scanpay--captureonorderstatus--dropdown').toggleClass('scanpay--show');
+    document.getElementById('scanpay--captureonorderstatus--add').addEventListener('click', function (e) {
+        dropdown.classList.toggle('scanpay--show');
         e.stopPropagation();
     });
 
@@ -21,29 +23,30 @@ $(document).ready(function () {
         span.textContent = '\u00D7';
         span.addEventListener('click', function (e) {
             li.parentNode.removeChild(li);
-            $('#SCANPAY_CAPTURE_ON_ORDER_STATUS\\[\\] option[value="' + id + '"]').removeAttr('selected');
+            document.querySelector('#SCANPAY_CAPTURE_ON_ORDER_STATUS\\[\\] option[value="' + id + '"]').removeAttribute('selected');
             addBtn.classList.remove('scanpay--usedstatus');
         });
         li.appendChild(span);
-        $('#scanpay--captureonorderstatus--list').append(li);
+        document.getElementById('scanpay--captureonorderstatus--list').appendChild(li);
         addBtn.classList.add('scanpay--usedstatus');
     }
 
-    $('#SCANPAY_CAPTURE_ON_ORDER_STATUS\\[\\] option').each(function (_i, opt) {
+    document.querySelectorAll('#SCANPAY_CAPTURE_ON_ORDER_STATUS\\[\\] option').forEach(function (opt) {
         var li = document.createElement('li');
-        li.textContent = opt.textContent
+        li.textContent = opt.textContent;
         li.addEventListener('click', function () {
             if (li.classList.contains('scanpay--usedstatus')) { return; }
-            $('#SCANPAY_CAPTURE_ON_ORDER_STATUS\\[\\] option[value="' + opt.value + '"]').attr('selected', '');
+            document.querySelector('#SCANPAY_CAPTURE_ON_ORDER_STATUS\\[\\] option[value="' + opt.value + '"]').setAttribute('selected', '');
             mkStatus(opt.value, opt.textContent, li);
         });
-        $('#scanpay--captureonorderstatus--dropdown').append(li);
+        dropdown.appendChild(li);
         if (opt.selected) {
             mkStatus(opt.value, opt.textContent, li);
         }
     });
 
-    $('#scanpay--pingurl--input').click(function () {
+    // Click should select all text in the input field
+    document.getElementById('scanpay--pingurl--input').addEventListener('click', function () {
         this.focus();
         this.select();
     });
